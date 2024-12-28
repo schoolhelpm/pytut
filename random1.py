66fbdd83392bebf00c1445b356cbf84b9c94296a
schoolhelpm <schoolhelpmentor@gmail.com>
2024-12-28 06:48:24 +0000

    test file - random

---
+++ random1.py
@@ -0,0 +1,8 @@
+# there are 25 employees in office, need to give 3 employee at random a special gift
+import random
+
+a=random.randint(1,25)
+b=random.randint(1,25)
+c=random.randint(1,25)
+
+print("These employees are selected at random:",a,b,c)
